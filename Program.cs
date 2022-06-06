5cfd5766c92aa67ae5b001f3d3e644cc974e7753
Matt Moore <matthew.moore3@tc.gc.ca>
2022-06-06 11:12:39 -0400

    first week of generics

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿var list = new MattsList<Hero>();
+var hero1 = new Hero();
+hero1.Id = "Echo";
+hero1.Image = "lkdsajflkajflksajdflkjdsalkfjsdlf";
+hero1.Name = "Echo";
+hero1.Description = "Echo is the annoying iPhone that flies around.";
+hero1.Role = "DPS";
+
+var hero2 = new Hero();
+hero2.Id = "Mercy";
+hero2.Image = "lkdsajflkajflksajdflkjdsalkfjsdlf";
+hero2.Name = "Mercy";
+hero2.Description = "Mercy is the best healer in the game who also flies around";
+hero2.Role = "Support";
+hero2.Secondary = "Healer";
+
+list.Add(hero1);
+list.Add(hero2);
+Console.WriteLine(list.ToString());
