08824ddf1e28a7070370d88f61b45a2f0e69f130
Matt Moore <matthew.moore3@tc.gc.ca>
2022-06-14 13:18:56 -0400

    removed build stuff

--- MattsList.cs
+++ MattsList.cs
@@ -13,21 +13,24 @@ class MattsList<T>
     {
         array = array.Append(item).ToArray<T>();
     }
-    
+
     // TODO: See if there is a faster way than using Reflection
     public override string ToString()
     {
         var output = new StringBuilder();
-        foreach(T item in array) {
-            
+        foreach (T item in array)
+        {
+
             // if null move to next item in array
-            if (item == null) {
+            if (item == null)
+            {
                 continue;
             }
 
             // loop through properties of item
-            foreach(PropertyInfo property in item.GetType().GetProperties()) {
-                output.Append(property.Name + ":"+ property.GetValue(item) + ",");
+            foreach (PropertyInfo property in item.GetType().GetProperties())
+            {
+                output.Append(property.Name + ":" + property.GetValue(item) + ",");
             }
         }
 
