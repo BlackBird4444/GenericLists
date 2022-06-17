1354c11c7109fdaa712a5379b43544347eed4b60
Matt Moore <matthew.moore3@tc.gc.ca>
2022-06-17 11:13:36 -0400

    working on understand Equals() inheritance

---
+++ GenericList.cs
@@ -0,0 +1,67 @@
+using System.Text;
+using System.Reflection;
+
+class GenericList<T>
+{
+    private T[] array;
+    public GenericList()
+    {
+        array = Array.Empty<T>();
+    }
+
+    public void Add(T item)
+    {
+        // TODO: Understand how badly this performs as we are creating a new array every call to Add()
+        //       How does IList<> do this in a performant way?  Is it backed by an array or something else?
+        array = array.Append(item).ToArray<T>();
+    }
+
+    public void Remove(T item)
+    {
+        var itemToRemove = array.First<T>(x =>
+        {
+            return x.Equals(item);
+        });
+        var newArray = Array.Empty<T>();
+        foreach (var thing in array)
+        {
+            if (!thing.Equals(itemToRemove))
+            {
+                newArray = newArray.Append(thing).ToArray<T>();
+            }
+        }
+        array = newArray;
+    }
+
+    public void Set(T item, int index)
+    {
+        array[index] = item;
+    }
+
+    public int Size()
+    {
+        return array.Length;
+    }
+
+    // TODO: See if there is a faster way than using Reflection
+    public override string ToString()
+    {
+        var output = new StringBuilder();
+        foreach (T item in array)
+        {
+
+            // if null move to next item in array
+            if (item == null)
+            {
+                continue;
+            }
+
+            // loop through properties of item
+            foreach (PropertyInfo property in item.GetType().GetProperties())
+            {
+                output.Append(property.Name + ":" + property.GetValue(item) + ",");
+            }
+        }
+        return output.ToString();
+    }
+}
