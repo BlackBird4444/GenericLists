3dcb7603c2c267fc7177cad5eacffb9aa312dd23
caroline.gignac <caroline.gignac@tc.gc.ca>
2022-07-29 09:21:04 -0400

    fixing repo

---
+++ GenericList.cs
@@ -0,0 +1,78 @@
+using System.Text;
+using System.Reflection;
+
+class GenericList<T>
+{
+    private T[] array;
+    public GenericList()
+    {
+        array = Array.Empty<T>();
+    }
+
+    public void Add(T item)
+    {
+        if (item == null)
+        {
+            throw new ArgumentException("GenericList does not allow for null values to be added.");
+        }
+        // TODO: Understand how badly this performs as we are creating a new array every call to Add()
+        //       How does IList<> do this in a performant way?  Is it backed by an array or something else?
+        array = array.Append(item).ToArray<T>();
+    }
+
+    public void Remove(T item)
+    {
+        if (item == null)
+        {
+            throw new ArgumentException("GenericList does not allow for null values to be added.");
+        }
+
+        var itemToRemove = array.FirstOrDefault<T>(x =>
+        {
+            return x!.Equals(item);
+        });
+
+        if (itemToRemove == null)
+        {
+            return;
+        }
+
+        // TODO: Is there a better way than enumerating?
+        var newArray = Array.Empty<T>();
+        foreach (var thing in array)
+        {
+            if (thing != null && !thing.Equals(itemToRemove))
+            {
+                newArray = newArray.Append(thing).ToArray<T>();
+            }
+        }
+        array = newArray;
+    }
+
+    public int Size()
+    {
+        return array.Length;
+    }
+
+    // TODO: See if there is a faster way than using Reflection
+    public override string ToString()
+    {
+        var output = new StringBuilder();
+        foreach (T item in array)
+        {
+
+            // if null move to next item in array
+            if (item == null)
+            {
+                continue;
+            }
+
+            // loop through properties of item
+            foreach (PropertyInfo property in item.GetType().GetProperties())
+            {
+                output.Append(property.Name + ":" + property.GetValue(item) + ",");
+            }
+        }
+        return output.ToString();
+    }
+}
--- Hero.cs
+++ Hero.cs
@@ -1,9 +1,51 @@
-class Hero
+class Hero : object
 {
-    public string? Id { get; set; }
+    public string Id { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
     public string? Role { get; set; }
     public string? Secondary { get; set; }
     public string? Image { get; set; }
+
+    public Hero(string id)
+    {
+        if (string.IsNullOrEmpty(id))
+        {
+            throw new NullReferenceException();
+        }   
+        //TODO: what type of method is IsNullOrEmpty? 
+        this.Id = id;
+        
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+        {
+            throw new NullReferenceException();
+        }
+
+        Hero? tempObj = null;
+
+        try
+        {
+            tempObj = (Hero)obj;
+        }
+        catch
+        {
+            return false;
+        }
+
+        if (tempObj.Id == this.Id)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return Int32.Parse(this.Id);
+    }
 }
